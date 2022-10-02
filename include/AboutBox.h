647be3c5d0f669753f1dda2aabecd78815cb0960
D.A.Tiger <drakarax@seznam.cz>
2022-10-02 17:04:19 +0200

    Add files description

--- include/AboutBox.h
+++ include/AboutBox.h
@@ -1,3 +1,26 @@
+/***************************************************************************************************
+* File:      AboutBox                                                                              *
+* Project:   Morfeus                                                                               *
+* Copyright: Copyriright (C) 2019 - 2022 by D.A.Tiger GNU GPL v3                                   *
+* Author(s): D.A.Tiger ( drakarax@seznam.cz )                                                      *
+* Note:      The window dialog with informations on Morfeus                                        *
+***************************************************************************************************/
+#ifndef ABOUTBOX_H_INCLUDED
+#define ABOUTBOX_H_INCLUDED
+/*************************************************************************
+* This program is free software: you can redistribute it and/or modify   *
+* it under the terms of the GNU General Public License as published by   *
+* the Free Software Foundation, either version 3 of the License, or      *
+* (at your option) any later version.                                    *
+*                                                                        *
+* This program is distributed in the hope that it will be useful,        *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+* GNU General Public License for more details.                           *
+*                                                                        *
+* You should have received a copy of the GNU General Public License      *
+* along with this program.  If not, see <https://www.gnu.org/licenses/>. *
+*************************************************************************/
 #include<define.h>
 #include<fxghi.h>
 
@@ -14,3 +37,5 @@ protected:
   AboutBox( ) { }
 
 };
+
+#endif /* ABOUTBOX_H_INCLUDED */
--- include/Morfeus.h
+++ include/Morfeus.h
@@ -1,6 +1,26 @@
+/***************************************************************************************************
+* File:      Morfeus                                                                               *
+* Project:   Morfeus                                                                               *
+* Copyright: Copyriright (C) 2019 - 2022 by D.A.Tiger GNU GPL v3                                   *
+* Author(s): D.A.Tiger ( drakarax@seznam.cz )                                                      *
+* Note:      The main window for Morfeus application                                               *
+***************************************************************************************************/
 #ifndef MORFEUS_H_INCLUDED
 #define MORFEUS_H_INCLUDED
-
+/*************************************************************************
+* This program is free software: you can redistribute it and/or modify   *
+* it under the terms of the GNU General Public License as published by   *
+* the Free Software Foundation, either version 3 of the License, or      *
+* (at your option) any later version.                                    *
+*                                                                        *
+* This program is distributed in the hope that it will be useful,        *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+* GNU General Public License for more details.                           *
+*                                                                        *
+* You should have received a copy of the GNU General Public License      *
+* along with this program.  If not, see <https://www.gnu.org/licenses/>. *
+*************************************************************************/
 #include<define.h>
 #include<MW_App.h>
 #include<AboutBox.h>

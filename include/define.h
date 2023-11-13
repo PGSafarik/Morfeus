f8fd3c3e61e5547d2abfbfb8d46707523b0b5201
D.A.Tiger <drakarax@seznam.cz>
2023-11-13 13:36:13 +0100

    Adaption project on foxlib 1.7.84

--- include/define.h
+++ include/define.h
@@ -35,10 +35,10 @@
 namespace Morfeus_Version {
 	
 	//Date Version Types
-	static const char DATE[] = "23";
-	static const char MONTH[] = "05";
+	static const char DATE[] = "12";
+	static const char MONTH[] = "11";
 	static const char YEAR[] = "2023";
-	static const char UBUNTU_VERSION_STYLE[] =  "23.05";
+	static const char UBUNTU_VERSION_STYLE[] =  "23.11";
 	
 	//Software Status
 	static const char STATUS[] =  "ALPHA";
@@ -47,14 +47,14 @@ namespace Morfeus_Version {
 	//Standard Version Type
 	static const long MAJOR  = 0;
 	static const long MINOR  = 3;
-	static const long BUILD  = 1;
+	static const long BUILD  = 2;
 	static const long REVISION  = 2;
 	
 	//Miscellaneous Version Types
 	static const long BUILDS_COUNT  = 1;
-	#define RC_FILEVERSION 0,3,1,2
-	#define RC_FILEVERSION_STRING "0, 3, 1, 2\0"
-	static const char FULLVERSION_STRING [] = "0.3.1.2";
+	#define RC_FILEVERSION 0,3,2,2
+	#define RC_FILEVERSION_STRING "0, 3, 2, 2\0"
+	static const char FULLVERSION_STRING [] = "0.3.2.2";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY  = 0;
